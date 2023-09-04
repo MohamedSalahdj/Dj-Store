71df687cc7ff6cb69985241abd6755049359468a
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-04 17:31:42 +0300

    added productlist, detaillist, urls,created templates with simple front-end

--- project/urls.py
+++ project/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Products/',include('product.urls'))
 ]
 
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
