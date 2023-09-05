930e6cfdbf1f5de31ff38cf037c040bfc6f0bb2f
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-05 18:06:02 +0300

    added method to render reviews

--- product/views.py
+++ product/views.py
@@ -10,4 +10,10 @@ class ProductDetail(DetailView):
     model = Product    
     context_object_name = 'product'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["review"] = Review.objects.filter(Product=self.get_object())
+        return context
+    
+
 
