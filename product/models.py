671b8d1a002a4cfd477d17647bc983f398a6c182
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-07 23:19:46 +0300

    added slug field to Brand Model

--- product/models.py
+++ product/models.py
@@ -42,11 +42,14 @@ class Product_Images(models.Model):
 class Brand(models.Model):
     name = models.CharField(_('Name'), max_length=70)
     image = models.ImageField(_('Image'), upload_to='brands')
+    slug = models.SlugField()
 
     def __str__(self):
         return self.name
 
-
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Brand, self).save(*args, **kwargs)
 
 
 class Review(models.Model):
