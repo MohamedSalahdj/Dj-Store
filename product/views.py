a8e58c4d7afae4b9085636cfe316055203178d46
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-07 17:42:03 +0300

    added related item query, show it in templates

--- product/views.py
+++ product/views.py
@@ -13,6 +13,7 @@ class ProductDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["reviews"] = Review.objects.filter(Product=self.get_object())
+        context["related_items"] = Product.objects.filter(brand=self.get_object().brand)
         return context
     
 
