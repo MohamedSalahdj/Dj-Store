4748ba7c535e3dfc9d47d2a6c5c75de823e86e4e
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-08 23:33:45 +0300

    added brand_detail, url

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProductList, ProductDetail, BrandList
+from .views import ProductList, ProductDetail, BrandList, BrandDetail
 
 urlpatterns = [
     path('',ProductList.as_view()),
     path('<slug:slug>',ProductDetail.as_view()),
     path('brand/', BrandList.as_view()),
+    path('brand/<slug:slug>/', BrandDetail.as_view()),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,8 @@
+from typing import Any, Dict
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product, Product_Images, Brand, Review
+
 
 class ProductList(ListView):
     model = Product
@@ -15,8 +17,23 @@ class ProductDetail(DetailView):
         context["reviews"] = Review.objects.filter(Product=self.get_object())
         context["related_items"] = Product.objects.filter(brand=self.get_object().brand)
         return context
-    
 
 
 class BrandList(ListView):
     model = Brand
+
+
+class BrandDetail(ListView):
+    model = Product
+    template_name = 'product/brand_detail.html'
+
+    def get_queryset(self):
+        brand = Brand.objects.get(slug=self.kwargs['slug'])
+        return super().get_queryset().filter(brand=brand)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['brand'] = Brand.objects.get(slug = self.kwargs['slug'])
+        return context
+
+    
