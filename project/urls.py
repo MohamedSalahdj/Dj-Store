eeffa5b8f04422b76a48fa1e3142e90f23485e5c
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-09-16 10:09:24 +0300

    added home page data , url, templates

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Products/',include('product.urls')),
+    path('',include('settings.urls')),
+    path('products/',include('product.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Company
+from product.models import Product, Brand, Review
+from django.db.models.aggregates import Count
 
-# Create your views here.
+def home(request):
+    brands = Brand.objects.all().annotate(product_count=Count('brand_product'))
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+    feature_products = Product.objects.filter(flag='Feature')[:6]
+    new_products = Product.objects.filter(flag='New')[:8]
+    revviews = Review.objects.all()[:10]
+    
+    context = {
+        'brands': brands,
+        'sale_products': sale_products,
+        'feature_products': feature_products,
+        'new_products': new_products,
+        'revviews': revviews,
+    }
+    
+    return render(request, 'settings/home.html',context)
