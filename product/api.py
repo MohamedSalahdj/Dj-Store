a53ab5e618c6a2967b34f6f34b3a83786ff34669
MohamedSalahdj <mohamedsamydj@gmail.com>
2023-10-31 20:46:44 +0200

    added list api , detail api for products

--- product/api.py
+++ product/api.py
@@ -1,18 +1,31 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import generics
 from .serializers import ProductSerializer
 from .models import Product
 
 
-@api_view(['GET'])
-def product_list_api(request):
-    products = Product.objects.all()[:20] # list
-    data = ProductSerializer(products, context={'request':request}, many=True).data #json
-    return Response({'products':data})
+# @api_view(['GET'])
+# def product_list_api(request):
+#     products = Product.objects.all()[:20] # list
+#     data = ProductSerializer(products, context={'request':request}, many=True).data #json
+#     return Response({'products':data})
 
 
-@api_view(['GET'])
-def product_detail_api(request, pk):
-    product = Product.objects.get(id=pk) 
-    data = ProductSerializer(product, context={'request':request}).data
-    return Response({'product':data})
+# @api_view(['GET'])
+# def product_detail_api(request, pk):
+#     product = Product.objects.get(id=pk) 
+#     data = ProductSerializer(product, context={'request':request}).data
+#     return Response({'product':data})
+
+
+class PeoductListApi(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetailApi(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductList, ProductDetail, BrandList, BrandDetail, queryset_debug
-from .api import product_list_api, product_detail_api
+from .api import PeoductListApi, ProductDetailApi
 
 urlpatterns = [
     path('',ProductList.as_view()),
@@ -10,6 +10,6 @@ urlpatterns = [
     path('brand/<slug:slug>/', BrandDetail.as_view()),
 
     #api urls
-    path('api/list', product_list_api),
-    path('api/productdetail/<int:pk>', product_detail_api)
+    path('api/list', PeoductListApi.as_view()),
+    path('api/productdetail/<int:pk>', ProductDetailApi.as_view())
 ]
