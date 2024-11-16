8e4f60e00db4bcff3020fdfb60e2d35554d7c428
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-11-16 20:52:20 +0200

    added new unit test from brand model

--- product/tests/models/test_brand_model.py
+++ product/tests/models/test_brand_model.py
@@ -21,13 +21,13 @@ class BrandTestModel(TestCase):
         self.assertEqual(self.brand.name, "Apple Test")
 
     def test_brand_name_verbose_name(self):
-        """Verify the vernose name of brand name is correctly"""
+        """Verify the verbose name of brand name is correctly"""
         field_label = self.brand._meta.get_field('name').verbose_name 
         self.assertEqual(field_label, 'Name')
 
-    def test_brand_slug(self):
-        """Verify the brand slug starts with a lowercase letter and uses '-' between words."""
-        self.assertEqual(self.brand.slug, "apple-test")
+    def test_brand_name_max_length(self):
+        """Test that the max length of the 'name' field is 70."""
+        self.assertEqual(self.brand._meta.get_field('name').max_length, 70)
     
     def test_brand_image_exists(self):
         """
@@ -36,5 +36,15 @@ class BrandTestModel(TestCase):
         """
         self.assertTrue(self.brand.image.name.startswith("brands/"))
         self.assertIn("apple", self.brand.image.name)
+    
+    def test_brand_image_verbose_name(self):
+        """Test that the verbose name of the 'image' field is 'Image'."""
+        self.assertEqual(self.brand._meta.get_field('image').verbose_name, "Image")
 
+    def test_brand_slug(self):
+        """Verify the brand slug starts with a lowercase letter and uses '-' between words."""
+        self.assertEqual(self.brand.slug, "apple-test")
 
+    def test_brand_str_method(self):
+        """Test the __str__ method of the brand model returns 'Apple Test'."""
+        self.assertEqual(str(self.brand), "Apple Test")
