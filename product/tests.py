11b7bd29470b693ca6cba11ae07ee21e5e28b6ac
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-11-16 16:58:23 +0200

    added two unit test , - test_brand_slug - test_brand_image_exists

--- product/tests.py
+++ product/tests.py
@@ -11,11 +11,18 @@ class BrandTestModel(TestCase):
                 name='apple.png', 
                 content=img.read(),
                 content_type='image/png')
-            Brand.objects.create(
-                name='Apple',
+            cls.brand = Brand.objects.create(
+                name='Apple Test',
                 image=cls.image
             )
 
     def test_brand_name(self):
-        brand = Brand.objects.get(id=1)
-        self.assertEqual(brand.name, "Apple")
+        self.assertEqual(self.brand.name, "Apple Test")
+
+    def test_brand_slug(self):
+        self.assertEqual(self.brand.slug, "apple-test")
+    
+    def test_brand_image_exists(self):
+        self.assertTrue(self.brand.image.name.startswith("brands/"))
+        self.assertIn("apple", self.brand.image.name)
+
