270c4ddeff5253259fbc87931fc38a2d8743ff11
MohamedSalahdj <mohamedsamydj@gmail.com>
2024-11-16 16:26:34 +0200

    added - `images-tests` folder that include images will use it in test - BrandTestModel class with one unit test

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
+from .models import Brand, Product
 
-# Create your tests here.
+
+class BrandTestModel(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        with open(r'D:/Courses/Django-fullstack-Course/Django-project/Dj-Store/src/images-tests/apple.png', 'rb') as img:
+            cls.image = SimpleUploadedFile(
+                name='apple.png', 
+                content=img.read(),
+                content_type='image/png')
+            Brand.objects.create(
+                name='Apple',
+                image=cls.image
+            )
+
+    def test_brand_name(self):
+        brand = Brand.objects.get(id=1)
+        self.assertEqual(brand.name, "Apple")
